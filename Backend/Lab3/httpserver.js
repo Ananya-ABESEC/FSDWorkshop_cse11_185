72f2038832d35944f003bb36769aa8b3bb762497
Ananya <a42922840@gmail.com>
2026-08-21 14:29:18 +0530

    Initial commit

---
+++ Backend/Lab3/httpserver.js
@@ -0,0 +1,7 @@
+import http, { createServer } from "http";
+const server = http.createServer((req, res)=> {
+
+});
+server.listen(3000, ()=> {
+    console.log("Server is running on code no. 3000")
+});
